e768c69cc6c12ec5e4e4aca8aac26f9fa14fcef3
Valeriy Seregin <valera-s-38@mail.ru>
2021-02-05 17:16:51 +0300

    connect MySQL

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -37,33 +37,17 @@
 
 <div class="container">
     <div class="starter-template">
-        <div class="panel">
-            <a href="/mobiles">
-                <img src="/storage/categories/mobile.jpg">
-                <h2>Мобильные телефоны</h2>
-            </a>
-            <p>
-                В этом разделе вы найдёте самые популярные мобильные телефонамы по отличным ценам!
-            </p>
-        </div>
-        <div class="panel">
-            <a href="/portable">
-                <img src="/storage/categories/portable.jpg">
-                <h2>Портативная техника</h2>
-            </a>
-            <p>
-                Раздел с портативной техникой.
-            </p>
-        </div>
-        <div class="panel">
-            <a href="/appliances">
-                <img src="/storage/categories/appliance.jpg">
-                <h2>Бытовая техника</h2>
-            </a>
-            <p>
-                Раздел с бытовой техникой
-            </p>
-        </div>
+        @foreach($categories as $category)
+            <div class="panel">
+                <a href="/{{ $category->code }}">
+                    <img src="{{ $category->image }}">
+                    <h2>{{ $category->name }}</h2>
+                </a>
+                <p>
+                    {{ $category->description }}
+                </p>
+            </div>
+        @endforeach
     </div>
 </div>
 </body>
