4406e864ab6a29543cbed135d1c608bc789251a0
Valeriy Seregin <valera-s-38@mail.ru>
2021-02-08 17:16:37 +0300

    use auth middleware

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $orders = Order::where('status', 1)->get();
+        return view('auth.orders.index', compact('orders'));
+    }
+}
