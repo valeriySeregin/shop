1cca5bf1a60004d0e9faca7963c46627208e16fe
Valeriy Seregin <valera-s-38@mail.ru>
2021-02-09 15:12:19 +0300

    add categories into admin panel

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,8 @@ class Category extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['code', 'name', 'description', 'image'];
+
     public function products()
     {
         return $this->hasMany(Product::class);
